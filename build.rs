6f44ce47653d21983bdc9f29e8756fe2e57ad080
newdaynaz <mgraham@newdaynaz.org>
2025-12-19 03:16:13 -0600

    revert windows specific  changes

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 fn main() {
     if cfg!(target_os = "windows") {
-        // Set Windows subsystem (no console by default)
-        println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
-        
         winres::WindowsResource::new()
             .set_icon("midi_showxpress_controller.ico")
             .compile()
