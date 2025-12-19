81a0c0852c4d64f69c754d329bdb1c87efa26074
NewDayNaz <mgraham@newdaynaz.org>
2025-12-19 03:49:02 -0600

    more build compat

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
+#[cfg(target_os = "windows")]
 fn main() {
-    if cfg!(target_os = "windows") {
-        winres::WindowsResource::new()
-            .set_icon("midi_showxpress_controller.ico")
-            .compile()
-            .unwrap();
-    }
+    winres::WindowsResource::new()
+        .set_icon("midi_showxpress_controller.ico")
+        .compile()
+        .unwrap();
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {
+    // No-op for non-Windows platforms
 }
 
